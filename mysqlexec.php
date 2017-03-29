92460c267074174791a170cb03a7beaeef904fe0
James <jawalker@oakland.edu>
2017-03-29 06:39:51 +0000

    setup feeding time insert sql

--- mysqlexec.php
+++ mysqlexec.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "10.0.1.183";
+$servername = "localhost";
 $username = "root";
-$password = "cit480";
+$password = "";
 $dbname = "Kibblebit";
 
 // Create connection
@@ -11,16 +11,27 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+$feedtime = date("D M d, Y G:i");
+
 $sql = "SELECT * FROM `Feedings` WHERE 1";
+//$sql2 = "INSERT INTO `Feeds`(`KibblebitID`, `Date`, `Notes`) VALUES ('12345'," .$feedtime . ",'fed')";
+
+//$insert = $conn->query($sql2);
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "FeedID: " . $row["FeedID"]. " - KibblebitID: " . $row["KibblebitID"]. " " . $row["Time"]. "<br>";
+
     }
 } else {
     echo "0 results";
 }
 $conn->close();
+
+//escapeshellcmd
+//$command = ('/var/www/html/testpython.py');
+//$output = shell_exec($command);
+
 ?>
